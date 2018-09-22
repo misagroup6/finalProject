48765829c39b0aebc126644e35a8e66b3b2b11c3
Lam Nguyen <lamnt437@gmail.com>
2018-09-22 09:09:25 +0700

    final v3

--- MISA_WDT_HeroDev_Nhom6/MISA_WDT_HeroDev_Nhom6/Scripts/js1.js
+++ MISA_WDT_HeroDev_Nhom6/MISA_WDT_HeroDev_Nhom6/Scripts/js1.js
@@ -10,20 +10,20 @@ $(document).ready(function () {
         var current = this;
         $(current).next().slideToggle("fast");
         $(current).next().siblings(".content-left-exten-more").slideUp("fast");
-    })
+    });
     $('[click="active"]').click(function () {
         var link = this;
         $(link).addClass("active");
         $(link).siblings().removeClass("active");
-    })
+    });
     $('[click="active1"]').click(function () {
         var link = this;
         $(link).addClass("active1");
         $(link).siblings().removeClass("active1");
-    })
+    });
     $('.click-btn').click(function () {
         $('.content-left-wrap').toggleClass("content-left-wrap-display");
-    })
+    });
     $('#danhmuc').click(function () {
         $('.content-left-sub').css("display", "flex");
         count++;
@@ -31,12 +31,12 @@ $(document).ready(function () {
             $('.content-left-sub').css("display", "none");
         }
         console.log(count);
-    })
+    });
     $('#btn-customer').click(function () {
         $('.content-right').css("display", "flex");
         $('.content-left-sub').css("display", "none");
         count--;
-    })
+    });
     $('#my-select').change(function () {
         end = $('#my-select').val();
         endInt = parseInt(end);
@@ -47,34 +47,34 @@ $(document).ready(function () {
         $('#current').val(1);
         $('#endRec').text(end);
         $('#totalPage').text("trên " + totalPage);
-    })
-    $('[click="btn-scroll-right"]').click(function() {
+    });
+    $('[click="btn-scroll-right"]').click(function () {
         event.preventDefault();
         $('.foothead-navi').animate({
             scrollLeft: "+=200px"
         }, "slow");
-     });
-  
-       $('[click="btn-scroll-left"]').click(function() {
+    });
+
+    $('[click="btn-scroll-left"]').click(function () {
         event.preventDefault();
         $('.foothead-navi').animate({
             scrollLeft: "-=200px"
         }, "slow");
-     });
+    });
 
-     $('#show-info').click(function () {
+    $('#show-info').click(function () {
         $('.content-right-footer-info-wrapper').css("display", "flex");
-    })
-    $('#hide-info').click(function () {
+    });
+    $('[hidee="hide-info"]').click(function () {
         $('.content-right-footer-info-wrapper').css("display", "none");
-    })
+    });
     $('#show-popup').click(function () {
         $('.content-add').css("display", "flex");
         $('.content-right').css("display", "none");
-    })
+    });
     $('#pa-closeAddContent').click(function () {
         $('.content-add').css("display", "none");
         $('.content-right').css("display", "flex");
-    })
-})
+    });
+});
 count = 0;
